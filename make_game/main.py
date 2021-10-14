c796025cbab5872d0a82c66d7ab61031fc27b022
chpink0518 <anny78@gmail.com>
2021-10-15 07:27:41 +0900

    make_game part 3

--- make_game/main.py
+++ make_game/main.py
@@ -11,13 +11,23 @@ win = pygame.display.set_mode((800, 400))
 # 7. change caption
 pygame.display.set_caption("YES의 게임")
 
+# 01. change icon
+icon = pygame.image.load('graphics/snail_icon.png')
+pygame.display.set_icon(icon)
+
 clock = pygame.time.Clock()
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 
 my_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 text_surface = my_font.render('My game', False, 'Black')
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_x_pos = 600
+
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(topleft = (80, 200))
 
 # 4. main loop
 while True:
@@ -30,6 +40,12 @@ while True:
     win.blit(sky_surface, (0,0)) 
     win.blit(ground_surface, (0,300)) 
     win.blit(text_surface, (330,50))
+    
+    snail_x_pos -= 4
+    if snail_x_pos < -100: snail_x_pos = 800
+    win.blit(snail_surface, (snail_x_pos,250))
+    
+    win.blit(player_surf, player_rect)
                  
     pygame.display.update()
     clock.tick(60)
