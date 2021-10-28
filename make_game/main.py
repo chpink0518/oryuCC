362f1f3a732ff225bafa2044fdf74ed85d43da8d
chpink0518 <anny78@gmail.com>
2021-10-29 06:14:17 +0900

    make game 4th

--- make_game/main.py
+++ make_game/main.py
@@ -17,17 +17,25 @@ pygame.display.set_icon(icon)
 
 clock = pygame.time.Clock()
 
+# background
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
+# font
 my_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 text_surface = my_font.render('My game', False, 'Black')
 
+# snail - enemy
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_rect = snail_surface.get_rect(bottomright = (600, 300))
 
+# player
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
-player_rect = player_surf.get_rect(topleft = (80, 200))
+player_rect = player_surf.get_rect(bottomleft = (80, 300))
+player_gravity = 0
+
+# for game variables
+gameActive = True
 
 # 4. main loop
 while True:
@@ -36,16 +44,39 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
-    win.blit(sky_surface, (0,0)) 
-    win.blit(ground_surface, (0,300)) 
-    win.blit(text_surface, (330,50))
-    
-    snail_x_pos -= 4
-    if snail_x_pos < -100: snail_x_pos = 800
-    win.blit(snail_surface, (snail_x_pos,250))
-    
-    win.blit(player_surf, player_rect)
+        #03. make event loop code for jump
+        if event.type == pygame.KEYDOWN: #키를 눌렀을 때, 뗐을 때는 KEYUP
+            if event.key == pygame.K_SPACE and player_rect.bottom == 300:
+                player_gravity = -25 
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1 and player_rect.bottom == 300:
+                if player_rect.collidepoint(event.pos):
+                    player_gravity = -25  
+                    
+                       
+    if gameActive:
+        win.blit(sky_surface, (0,0)) 
+        win.blit(ground_surface, (0,300)) 
+        win.blit(text_surface, (330,50))
+        
+        snail_rect.x -= 4
+        if snail_rect.right <= 0: snail_rect.left = 800
+        win.blit(snail_surface, snail_rect)
+        
+        player_gravity += 1
+        player_rect.y += player_gravity
+        
+        if player_rect.bottom > 300: 
+            player_rect.bottom = 300 
                  
+        win.blit(player_surf, player_rect)
+        
+        # collision
+        if snail_rect.colliderect(player_rect):
+            gameActive = False 
+    else:
+        win.fill((94, 129, 162))
+                    
     pygame.display.update()
     clock.tick(60)
